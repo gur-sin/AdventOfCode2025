d91abe525c1951c221ea9c792b1151a658312958
gur-sin <gurnoorjagjotsingh@gmail.com>
2025-12-02 17:51:54 -0500

    Day 2: Complete

---
+++ day2/part2.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkRepeat(start int, end int) []int {
+	invalidIDs := []int{}
+
+	for num := start; num <= end; num++ {
+		s := strconv.Itoa(num)
+		n := len(s)
+
+		for i := 1; i <= n/2; i++ {
+			if n%i == 0 {
+				segment := s[:i]
+				repeats := n / i
+				repeatedString := strings.Repeat(segment, repeats)
+
+				if repeatedString == s {
+					invalidIDs = append(invalidIDs, num)
+					break
+				}
+			}
+		}
+	}
+
+	return invalidIDs
+}
+
+func main() {
+	totalInvalidIDs := []int{}
+	sum := 0
+
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	fileContentInBytes, err := io.ReadAll(reader)
+	check(err)
+	fileContent := string(fileContentInBytes)
+	fileContent = strings.TrimSuffix(fileContent, "\n")
+
+	splitArray := strings.Split(fileContent, ",")
+	for _, idRange := range splitArray {
+		splitRange := strings.Split(idRange, "-")
+		start, err := strconv.Atoi(splitRange[0])
+		check(err)
+		end, err := strconv.Atoi(splitRange[1])
+		check(err)
+		invalidIDsInRange := checkRepeat(start, end)
+
+		for _, id := range invalidIDsInRange {
+			totalInvalidIDs = append(totalInvalidIDs, id)
+		}
+	}
+
+	for _, id := range totalInvalidIDs {
+		sum += id
+	}
+
+	fmt.Println(sum)
+}
